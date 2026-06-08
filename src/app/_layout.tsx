c0cc439f047b951a9ca94672b355cba89ff8b007
tim-rayner <tim.rayner2020@gmail.com>
2026-06-08 23:49:25 +0100

    init: initial expo commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
+import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/layout/app-tabs";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
       <AppTabs />
     </ThemeProvider>
---
+++ src/layout/themed/themed-view.tsx
@@ -0,0 +1,27 @@
+import { View, type ViewProps } from "react-native";
+
+import { useTheme } from "@/theme/hooks/use-theme";
+import { ThemeColor } from "@/theme/theme";
+
+export type ThemedViewProps = ViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+  type?: ThemeColor;
+};
+
+export function ThemedView({
+  style,
+  lightColor,
+  darkColor,
+  type,
+  ...otherProps
+}: ThemedViewProps) {
+  const theme = useTheme();
+
+  return (
+    <View
+      style={[{ backgroundColor: theme[type ?? "background"] }, style]}
+      {...otherProps}
+    />
+  );
+}
